d3eacce9f57831e6bc91f60622ecdbd95167b5fc
mgcarbonell <mgcarbonell@gmail.com>
2021-07-08 15:46:07 -0700

    Adds the flying saucer quote, adds the Gucci Mane adlib burr

--- gucci_quote.py
+++ gucci_quote.py
@@ -4,21 +4,22 @@ import random
 
 
 quote_list = [
-        "Ain't got no comparison and I ain't being arrogant.", 
-        "I don't give a damn how you feel about me...", 
-        "When all my hard work is getting recognized it makes me work even harder.", 
-        "I will keep on hurdling and keep on hustling.",
-        "Before I make an action, think about the consequences... Any decision I make is a heavy decision.",
-        "If a man does not have the sauce, then he is lost. But the same man can be lost in the sauce.",
-        "I feel like if I show people how good I'm doing with myself, then it'll bring people to want me to manage them as well.",
-        "I'm satisifed and proud of the things I did - even the bumps and the bruises that I've had on the way. You fall down, you get up, you brush yourself off and you keep going. And that's what we're doing.",
-        "A trapper but this rappin got me going places you'll never go!",
-        "I ball all through the winter and I stunt all through the summer.",
-        "If you keep lookin' back, you gon' trip going forward.",
-        "There are some things in life you can never completely walk away from, as badly as you might want to.",
-        "From Maine to Spain, I can play that thang, because I'm the original Gucci Mane.",
-        "I try to take a couple of young guys under my wing and tell them what I've learned, because you can't teach nobody if you don't know anything.",
-        ]
+    "Ain't got no comparison and I ain't being arrogant.",
+    "I don't give a damn how you feel about me...",
+    "When all my hard work is getting recognized it makes me work even harder.",
+    "I will keep on hurdling and keep on hustling.",
+    "Before I make an action, think about the consequences... Any decision I make is a heavy decision.",
+    "If a man does not have the sauce, then he is lost. But the same man can be lost in the sauce.",
+    "I feel like if I show people how good I'm doing with myself, then it'll bring people to want me to manage them as well.",
+    "I'm satisifed and proud of the things I did - even the bumps and the bruises that I've had on the way. You fall down, you get up, you brush yourself off and you keep going. And that's what we're doing.",
+    "A trapper but this rappin got me going places you'll never go!",
+    "I ball all through the winter and I stunt all through the summer.",
+    "If you keep lookin' back, you gon' trip going forward.",
+    "There are some things in life you can never completely walk away from, as badly as you might want to.",
+    "From Maine to Spain, I can play that thang, because I'm the original Gucci Mane.",
+    "I try to take a couple of young guys under my wing and tell them what I've learned, because you can't teach nobody if you don't know anything.",
+    "This imported foreign flying saucer make me feel important.",
+    "Burr!",
+]
 
 print(f'"{random.choice(quote_list)}"\n - Gucci Mane')
-
